eb9bb704db938a885a7389f1eca8b1c10bb32e1b
SUdip <kumarsudip053@gmail.com>
2025-10-07 23:03:04 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base:'/info-key/',
+  base:process.env.VITE_BASE_PATH || "/info_key",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
